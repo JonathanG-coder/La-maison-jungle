c757d70d4339228a1071b1a4c0fd88b74b5b0d3e
Jonathan <jonathangager75@gmail.com>
2025-01-22 14:27:12 +0100

    Add Logo and Style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,17 @@
 import './Banner.css';
+import logo from '../../assets/logo.png';
+
+
+
 
 function Banner() {
-    const title = "La maison jungle";
-    return <h1>{title.toUpperCase()}</h1>;
+    const title = 'La maison jungle'
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>{title}</h1>
+        </div>
+    )
 }
 
-export default Banner;
+export default Banner
