b6d8cc49e338d6b40005c12271526b21d456e695
Jonathan <jonathangager75@gmail.com>
2025-01-23 11:25:08 +0100

    Add style to ShopipingList

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,21 +1,31 @@
 import './ShoppingList.css';
+import { plantList } from '../../datas/plantList'
 
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+
 
 function ShoppingList() {
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>     //key : key={`${plant}-${index}`} 
-            ))}
-        </ul>
-    )
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
