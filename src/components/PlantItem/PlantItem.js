679d1ca1d4b9ecb97f936e601ac0d320a9fd5a96
Jonathan <jonathangager75@gmail.com>
2025-01-30 10:37:21 +0100

    Restructuring of PlantItem.Js

--- src/components/PlantItem/PlantItem.js
+++ src/components/PlantItem/PlantItem.js
@@ -3,10 +3,13 @@ import '../PlantItem/PlantItem.css'
 
 
 
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
 
-function PlantItem({ id, cover, name, water, light }) {
+function PlantItem({ cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -16,10 +19,5 @@ function PlantItem({ id, cover, name, water, light }) {
 		</li>
 	)
 }
-
-function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
-}
-
 
 export default PlantItem
