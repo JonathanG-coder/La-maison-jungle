b41ac220c90dbcc2e5baee622cd6bd1998bd2b1f
Jonathan <jonathangager75@gmail.com>
2025-01-30 10:18:17 +0100

    Add Function hanDleClick(plantName)

--- src/components/PlantItem/PlantItem.js
+++ src/components/PlantItem/PlantItem.js
@@ -2,9 +2,11 @@ import CareScale from '../CareScale/CareScale'
 import '../PlantItem/PlantItem.css'
 
 
+
+
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -14,5 +16,10 @@ function PlantItem({ id, cover, name, water, light }) {
 		</li>
 	)
 }
+
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
 
 export default PlantItem
