917bf3ee235374496c124cf15fcf58c29ff3d05f
Jonathan <jonathangager75@gmail.com>
2025-02-03 09:32:56 +0100

    Updated ShoppingList with new plant images and improved styling to ShoppingList

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,68 +1,87 @@
+import monstera from '../assets/monstera.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+
 export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-		water: 2,
-		light: 2
+		light: 2,
+		water: 3,
+		cover: monstera
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-		water: 3,
-		light: 3
+		light: 3,
+		water: 1,
+		cover: lyrata
 	},
+
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-		isSpecialOffer: true,
-		water: 1,
-		light: 1
+		light: 1,
+		water: 2,
+		cover: pothos
 	},
 	{
-		name: 'yucca',
+		name: 'calathea',
 		category: 'classique',
 		id: '4kk',
-		water: 1,
-		light: 3
+		light: 2,
+		water: 3,
+		cover: calathea
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-		water: 2,
-		light: 3
+		light: 3,
+		water: 1,
+		cover: olivier
 	},
+
 	{
-		name: 'géranium',
-		category: 'extérieur',
-		id: '6uo',
-		water: 3,
-		light: 2
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isSpecialOffer: true,
+		light: 2,
 		water: 3,
-		light: 2
-	},
-	{
-		name: 'aloe',
-		category: 'plante grasse',
-		id: '8fp',
-		isSpecialOffer: true,
-		water: 1,
-		light: 1
+		cover: basil
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
+		light: 2,
 		water: 1,
-		light: 1
+		cover: succulent
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint
 	}
-];
+]
