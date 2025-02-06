884fcf4bf2326921cb9127f5235060b60de45311
Jonathan <jonathangager75@gmail.com>
2025-02-06 10:20:59 +0100

    addition of cart management with quantity in the Cart component and improvement of the ShoppingList

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -2,26 +2,48 @@ import { plantList } from '../../datas/plantList'
 import PlantItem from '../PlantItem/PlantItem'
 import '../ShoppingList/ShoppingList.css'
 
-function ShoppingList({ cart, updateCart }) {
-	
+
+
+	function ShoppingList({ cart, updateCart }) {
 		const categories = plantList.reduce(
-			(acc, elem) =>
-				acc.includes(elem.category) ? acc : acc.concat(elem.category),
-				[]
+			(acc, plant) =>
+				acc.includes(plant.category) ? acc : acc.concat(plant.category),
+			[]
 		)
-		
+	
+		function addToCart(name, price) {
+			const currentPlantSaved = cart.find((plant) => plant.name === name)
+			if (currentPlantSaved) {
+				const cartFilteredCurrentPlant = cart.filter(
+					(plant) => plant.name !== name
+				)
+				updateCart([
+					...cartFilteredCurrentPlant,
+					{ name, price, amount: currentPlantSaved.amount + 1 }
+				])
+			} else {
+				updateCart([...cart, { name, price, amount: 1 }])
+			}
+		}
+	
 		return (
 			<div className='lmj-shopping-list'>
 				<ul>
 					{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
+						<li key={cat}>{cat}</li>
 					))}
 				</ul>
 				<ul className='lmj-plant-list'>
-					{plantList.map(({ id, cover, name, water, light }) => (
+					{plantList.map(({ id, cover, name, water, light, price }) => (
 						<div key={id}>
-							<PlantItem cover={cover} name={name} water={water} light={light} />
-							<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+							<PlantItem
+								cover={cover}
+								name={name}
+								water={water}
+								light={light}
+								price={price}
+							/>
+							<button onClick={() => addToCart(name, price)}>Ajouter</button>
 						</div>
 					))}
 				</ul>
